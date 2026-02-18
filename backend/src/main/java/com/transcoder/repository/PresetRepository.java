c22e28c171deb4ec963e3fab1bf132e9cbe71a3f
Onur-Akman <kartalonur11@gmail.com>
2026-02-18 13:45:07 +0300

    refactor: separate TranscodeService into interface and implementation

--- backend/src/main/java/com/transcoder/repository/PresetRepository.java
+++ backend/src/main/java/com/transcoder/repository/PresetRepository.java
@@ -2,8 +2,6 @@ package com.transcoder.repository;
 
 import com.transcoder.model.EncodingPreset;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PresetRepository extends JpaRepository<EncodingPreset, Long> {
 }
